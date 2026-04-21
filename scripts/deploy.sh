de5e4611267927155f11bcff66eeab857d7518a2
Trey Mack <tmack10@gmail.com>
2026-04-21 12:55:23 -0400

    Update permissions and refine deploy script for S3 sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 aws s3 sync . s3://stylesmalone.com-site-899504616761-us-east-1-an \
-  --exclude ".git/*" \
-  --exclude "*.md" \
-  --exclude "scripts/*" \
+  --exclude "*" \
+  --include "*.html" \
+  --include "*.css" \
+  --include "images/*" \
+  --include "images/icons/*" \
+  --include "songs/*" \
   --delete
